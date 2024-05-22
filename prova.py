8f8692d5a6c3b739a2704fa412497a8125d72162
ARWEmelcat <waraichabdullah@gmail.com>
2024-05-22 12:37:48 +0200

    ARW

--- prova.py
+++ prova.py
@@ -2,3 +2,8 @@ print("hola1")
 print("h111111111111111111111111111111111111111111")
 print("adeu")
 print("ofjerfhbbbbbbbbbbbbbbbbbbb")
+
+print("hola1")
+print("h111111111111111111111111111111111111111111")
+print("adeu")
+print("ofjerfhbbbbbbbbbbbbbbbbbbb")
